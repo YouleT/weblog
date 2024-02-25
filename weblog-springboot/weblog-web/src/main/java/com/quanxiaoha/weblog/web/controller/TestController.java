3c5976f0aaab96199d5f8715fb7571ca9e4fe9ea
YouleT <youleiye@gmail.com>
2024-02-25 09:07:28 +0800

    完成后端工程搭建

--- weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
+++ weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
@@ -1,11 +1,26 @@
 package com.quanxiaoha.weblog.web.controller;
 
+import com.quanxiaoha.weblog.common.enums.ResponseCodeEnum;
+import com.quanxiaoha.weblog.common.exception.BizException;
+import com.quanxiaoha.weblog.common.utils.JsonUtil;
+import com.quanxiaoha.weblog.common.utils.Response;
 import com.quanxiaoha.weblog.web.model.User;
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.stream.Collectors;
 
 /**
  * @author: 犬小哈
@@ -13,14 +28,50 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2023-08-10 10:34
  * @description: TODO
  **/
+@Api(tags = "首页模块")
 @RestController
 @Slf4j
 public class TestController {
 
+    /*@PostMapping("/test")
+    @ApiOperationLog(description = "测试接口")
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return Response.fail(errorMsg);
+        }
+
+        // 返参
+        return Response.success();
+    }*/
+
+    /*@ApiOperation(value = "测试接口")
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
-        // 返参
-        return user;
+    public Response test(@RequestBody @Validated User user) {
+        // 主动定义一个运行时异常，分母不能为零
+        int i = 1 / 0;
+        return Response.success();
+    }*/
+
+    @PostMapping("/test")
+    @ApiOperationLog(description = "测试接口")
+    @ApiOperation(value = "测试接口")
+    public Response test(@RequestBody @Validated User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
+
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
+
+        return Response.success(user);
     }
 }
