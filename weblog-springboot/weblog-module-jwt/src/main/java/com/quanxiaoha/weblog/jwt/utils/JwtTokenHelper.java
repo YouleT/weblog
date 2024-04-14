893d1b3d4ce7b559a1dd513753282f11d5927a95
YouleT <youleiye@gmail.com>
2024-04-14 13:54:22 +0800

    SpringSecurity整合JWT: 实现接口鉴权

--- weblog-springboot/weblog-module-jwt/src/main/java/com/quanxiaoha/weblog/jwt/utils/JwtTokenHelper.java
+++ weblog-springboot/weblog-module-jwt/src/main/java/com/quanxiaoha/weblog/jwt/utils/JwtTokenHelper.java
@@ -33,70 +33,15 @@ public class JwtTokenHelper implements InitializingBean {
      * 秘钥
      */
     private Key key;
-
     /**
      * JWT 解析
      */
     private JwtParser jwtParser;
-
     /**
-     * 解码配置文件中配置的 Base 64 编码 key 为秘钥
-     *
-     * @param base64Key
+     * Token 失效时间（分钟）
      */
-    @Value("${jwt.secret}")
-    public void setBase64Key(String base64Key) {
-        key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(base64Key));
-    }
-
-
-    /**
-     * 初始化 JwtParser
-     *
-     * @throws Exception
-     */
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        // 考虑到不同服务器之间可能存在时钟偏移，setAllowedClockSkewSeconds 用于设置能够容忍的最大的时钟误差
-        jwtParser = Jwts.parserBuilder().requireIssuer(issuer)
-                .setSigningKey(key).setAllowedClockSkewSeconds(10)
-                .build();
-    }
-
-    /**
-     * 生成 Token
-     *
-     * @param username
-     * @return
-     */
-    public String generateToken(String username) {
-        LocalDateTime now = LocalDateTime.now();
-        // Token 一个小时后失效
-        LocalDateTime expireTime = now.plusHours(1);
-
-        return Jwts.builder().setSubject(username)
-                .setIssuer(issuer)
-                .setIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
-                .setExpiration(Date.from(expireTime.atZone(ZoneId.systemDefault()).toInstant()))
-                .signWith(key)
-                .compact();
-    }
-
-    /**
-     * 解析 Token
-     *
-     * @param token
-     * @return
-     */
-    public Jws<Claims> parseToken(String token) {
-        try {
-            return jwtParser.parseClaimsJws(token);
-        } catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException e) {
-            throw new BadCredentialsException("Token 不可用", e);
-        } catch (ExpiredJwtException e) {
-            throw new CredentialsExpiredException("Token 失效", e);
-        }
-    }
+    @Value("${jwt.tokenExpireTime}")
+    private Long tokenExpireTime;
 
     /**
      * 生成一个 Base64 的安全秘钥
@@ -116,5 +61,77 @@ public class JwtTokenHelper implements InitializingBean {
     public static void main(String[] args) {
         String key = generateBase64Key();
         System.out.println("key: " + key);
+    }
+
+    /**
+     * 解码配置文件中配置的 Base 64 编码 key 为秘钥
+     *
+     * @param base64Key
+     */
+    @Value("${jwt.secret}")
+    public void setBase64Key(String base64Key) {
+        key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(base64Key));
+    }
+
+    /**
+     * 初始化 JwtParser
+     *
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 考虑到不同服务器之间可能存在时钟偏移，setAllowedClockSkewSeconds 用于设置能够容忍的最大的时钟误差
+        jwtParser = Jwts.parserBuilder().requireIssuer(issuer).setSigningKey(key).setAllowedClockSkewSeconds(10).build();
+    }
+
+    public String generateToken(String username) {
+        LocalDateTime now = LocalDateTime.now();
+        // 设置 Token 失效时间
+        LocalDateTime expireTime = now.plusMinutes(tokenExpireTime);
+
+        return Jwts.builder().setSubject(username).setIssuer(issuer).setIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant())).setExpiration(Date.from(expireTime.atZone(ZoneId.systemDefault()).toInstant())).signWith(key).compact();
+    }
+
+    /**
+     * 解析 Token
+     *
+     * @param token
+     * @return
+     */
+    public Jws<Claims> parseToken(String token) {
+        try {
+            return jwtParser.parseClaimsJws(token);
+        } catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+            throw new BadCredentialsException("Token 不可用", e);
+        } catch (ExpiredJwtException e) {
+            throw new CredentialsExpiredException("Token 失效", e);
+        }
+    }
+
+    /**
+     * 校验 Token 是否可用
+     *
+     * @param token
+     * @return
+     */
+    public void validateToken(String token) {
+        jwtParser.parseClaimsJws(token);
+    }
+
+    /**
+     * 解析 Token 获取用户名
+     *
+     * @param token
+     * @return
+     */
+    public String getUsernameByToken(String token) {
+        try {
+            Claims claims = jwtParser.parseClaimsJws(token).getBody();
+            String username = claims.getSubject();
+            return username;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
