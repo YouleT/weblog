a809aa2c70a0b1cd4c210729b088118077082675
YouleT <youleiye@gmail.com>
2024-04-25 22:01:11 +0800

    文章分类：删除接口开发等

--- weblog-springboot/weblog-module-admin/src/main/java/com/quanxiaoha/weblog/admin/service/AdminCategoryService.java
+++ weblog-springboot/weblog-module-admin/src/main/java/com/quanxiaoha/weblog/admin/service/AdminCategoryService.java
@@ -1,13 +1,33 @@
 package com.quanxiaoha.weblog.admin.service;
 
 import com.quanxiaoha.weblog.admin.model.vo.category.AddCategoryReqVO;
+import com.quanxiaoha.weblog.admin.model.vo.category.DeleteCategoryReqVO;
+import com.quanxiaoha.weblog.admin.model.vo.category.FindCategoryPageListReqVO;
+import com.quanxiaoha.weblog.common.utils.PageResponse;
 import com.quanxiaoha.weblog.common.utils.Response;
 
 public interface AdminCategoryService {
     /**
      * 添加分类
+     *
      * @param addCategoryReqVO
      * @return
      */
     Response addCategory(AddCategoryReqVO addCategoryReqVO);
+
+    /**
+     * 分类分页数据查询
+     *
+     * @param findCategoryPageListReqVO
+     * @return
+     */
+    PageResponse findCategoryList(FindCategoryPageListReqVO findCategoryPageListReqVO);
+
+    /**
+     * 删除分类
+     *
+     * @param deleteCategoryReqVO
+     * @return
+     */
+    Response deleteCategory(DeleteCategoryReqVO deleteCategoryReqVO);
 }
--- weblog-springboot/weblog-module-admin/src/main/java/com/quanxiaoha/weblog/admin/service/impl/AdminCategoryServiceImpl.java
+++ weblog-springboot/weblog-module-admin/src/main/java/com/quanxiaoha/weblog/admin/service/impl/AdminCategoryServiceImpl.java
@@ -1,17 +1,28 @@
 package com.quanxiaoha.weblog.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.quanxiaoha.weblog.admin.model.vo.category.AddCategoryReqVO;
+import com.quanxiaoha.weblog.admin.model.vo.category.DeleteCategoryReqVO;
+import com.quanxiaoha.weblog.admin.model.vo.category.FindCategoryPageListReqVO;
+import com.quanxiaoha.weblog.admin.model.vo.category.FindCategoryPageListRspVO;
 import com.quanxiaoha.weblog.admin.service.AdminCategoryService;
 import com.quanxiaoha.weblog.common.domain.dos.CategoryDO;
 import com.quanxiaoha.weblog.common.domain.mapper.CategoryMapper;
 import com.quanxiaoha.weblog.common.enums.ResponseCodeEnum;
 import com.quanxiaoha.weblog.common.exception.BizException;
+import com.quanxiaoha.weblog.common.utils.PageResponse;
 import com.quanxiaoha.weblog.common.utils.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -45,6 +56,69 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
 
         // 执行 insert
         categoryMapper.insert(insertCategoryDO);
+
+        return Response.success();
+    }
+
+    /**
+     * 分页查询方法
+     *
+     * @param findCategoryPageListReqVO
+     * @return
+     */
+    @Override
+    public PageResponse findCategoryList(FindCategoryPageListReqVO findCategoryPageListReqVO) {
+        // 获取当前页、以及每页需要展示的数据数量
+        Long current = findCategoryPageListReqVO.getCurrent();
+        Long size = findCategoryPageListReqVO.getSize();
+
+        // 分页对象(查询第几页、每页多少数据)
+        Page<CategoryDO> page = new Page<>(current, size);
+
+        // 构建查询条件
+        LambdaQueryWrapper<CategoryDO> wrapper = new LambdaQueryWrapper<>();
+
+        String name = findCategoryPageListReqVO.getName();
+        LocalDate startDate = findCategoryPageListReqVO.getStartDate();
+        LocalDate endDate = findCategoryPageListReqVO.getEndDate();
+
+        wrapper
+                // like 模块查询
+                .like(StringUtils.isNotBlank(name), CategoryDO::getName, name.trim())
+                // 大于等于 startDate
+                .ge(Objects.nonNull(startDate), CategoryDO::getCreateTime, startDate)
+                // 小于等于 endDate
+                .le(Objects.nonNull(endDate), CategoryDO::getCreateTime, endDate)
+                // 按创建时间倒叙
+                .orderByDesc(CategoryDO::getCreateTime);
+
+        // 执行分页查询
+        Page<CategoryDO> categoryDOPage = categoryMapper.selectPage(page, wrapper);
+
+        List<CategoryDO> categoryDOS = categoryDOPage.getRecords();
+
+        // DO 转 VO
+        List<FindCategoryPageListRspVO> vos = null;
+        if (!CollectionUtils.isEmpty(categoryDOS)) {
+            vos = categoryDOS.stream()
+                    .map(categoryDO -> FindCategoryPageListRspVO.builder()
+                            .id(categoryDO.getId())
+                            .name(categoryDO.getName())
+                            .createTime(categoryDO.getCreateTime())
+                            .build())
+                    .collect(Collectors.toList());
+        }
+
+        return PageResponse.success(categoryDOPage, vos);
+    }
+
+    @Override
+    public Response deleteCategory(DeleteCategoryReqVO deleteCategoryReqVO) {
+        // 分类 ID
+        Long categoryId = deleteCategoryReqVO.getId();
+
+        // 删除分类
+        categoryMapper.deleteById(categoryId);
 
         return Response.success();
     }
