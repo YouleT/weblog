eaec16834893b13e93468cb9bf0620ff9af1f8ab
YouleT <youleiye@gmail.com>
2024-05-05 14:03:54 +0800

    博客设置页：更新设置等

--- weblog-vue3/src/layouts/admin/components/AdminMenu.vue
+++ weblog-vue3/src/layouts/admin/components/AdminMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-slate-800 h-screen text-white menu-container transition-property-all"
+    <div class="fixed overflow-y-auto bg-slate-800 h-screen text-white menu-container transition-all duration-300"
         :style="{ width: menuStore.menuWidth }">
         <!-- 顶部 Logo, 指定高度为 64px, 和右边的 Header 头保持一样高 -->
         <div class="flex items-center justify-center h-[64px]">
@@ -24,17 +24,18 @@
 </template>
 
 <script setup>
-
+import { ref, computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useMenuStore } from '@/stores/menu'
-import { ref, computed } from 'vue'
+
+const menuStore = useMenuStore()
 
 const route = useRoute()
 const router = useRouter()
-// 引入 useMenuStore
-const menuStore = useMenuStore()
+
 // 是否折叠
 const isCollapse = computed(() => !(menuStore.menuWidth == '250px'))
+
 // 根据路由地址判断哪个菜单被选中
 const defaultActive = ref(route.path)
 
@@ -67,7 +68,7 @@ const menus = [
     {
         'name': '博客设置',
         'icon': 'Setting',
-        'path': '/admin/blog/setting',
+        'path': '/admin/blog/settings',
     },
 ]
 </script>
@@ -86,12 +87,19 @@ const menus = [
     background-color: #ffffff10;
 }
 
+
 .el-menu-item.is-active {
-    background-color: var(--el-color-primary);
+    background-color: #409eff10;
     color: #fff;
 }
 
-.el-menu-item.is-active:hover {
+.el-menu-item.is-active:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 2px;
+    height: 100%;
     background-color: var(--el-color-primary);
 }
 
